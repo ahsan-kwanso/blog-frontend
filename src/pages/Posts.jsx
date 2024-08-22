519ff4172a7dfdbdb59ec2e9b620a45038082948
ahsan-kwanso <m.ahsan@kwanso.com>
2024-08-22 11:53:58 +0500

    no post message added

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,135 +1,3 @@
-// import React from "react";
-// import { useSearchParams } from "react-router-dom";
-// import {
-//   Container,
-//   Box,
-//   Pagination,
-//   Snackbar,
-//   Alert,
-//   Typography,
-// } from "@mui/material";
-// import PostList from "../components/PostList";
-// import useFetchPosts from "../hooks/useFetchPosts";
-// import useFetchSearchPosts from "../hooks/useFetchSearchPosts";
-// import { defaultPage, defaultLimit } from "../utils/pagination";
-// import SearchField from "../components/SearchField";
-// import CreatePostButton from "../components/CreatePostButton";
-// import Sidebar from "../components/Sidebar";
-
-// const Posts = () => {
-//   const [searchParams, setSearchParams] = useSearchParams();
-//   const page = parseInt(searchParams.get("page")) || defaultPage;
-//   const limit = parseInt(searchParams.get("limit")) || defaultLimit;
-//   const searchQuery = searchParams.get("search") || "";
-//   const isMyPosts = searchParams.get("filter") === "my-posts";
-
-//   const {
-//     posts: postsSearch,
-//     total: totalSearch,
-//     isLoading: isLoadingSearch,
-//     error: errorSearch,
-//   } = useFetchSearchPosts(searchQuery, page, limit, isMyPosts);
-//   const {
-//     posts: postsDefault,
-//     total: totalDefault,
-//     isLoading: isLoadingDefault,
-//     error: errorDefault,
-//   } = useFetchPosts(isMyPosts, page, limit);
-
-//   const posts = searchQuery ? postsSearch : postsDefault;
-//   const total = searchQuery ? totalSearch : totalDefault;
-//   const isLoading = searchQuery ? isLoadingSearch : isLoadingDefault;
-//   const error = searchQuery ? errorSearch : errorDefault;
-
-//   const handlePageChange = (event, value) => {
-//     const newParams = { page: value, limit };
-
-//     if (searchQuery) {
-//       newParams.search = searchQuery;
-//     }
-//     if (isMyPosts) {
-//       newParams.filter = "my-posts";
-//     }
-
-//     setSearchParams(newParams);
-//   };
-
-//   return (
-//     <Box sx={{ display: "flex" }}>
-//       <Sidebar />
-//       <Container
-//         component="main"
-//         maxWidth="xl"
-//         sx={{ marginLeft: { xs: 7, sm: 10 }, flexGrow: 1 }}
-//       >
-//         <Box
-//           sx={{
-//             padding: { xs: 0, sm: 4 },
-//           }}
-//         >
-//           <Box
-//             sx={{
-//               top: 64,
-//               zIndex: 1,
-//               backgroundColor: "inherit",
-//               display: "flex",
-//               justifyContent: "space-between",
-//               alignItems: "center",
-//               mb: 2,
-//               mt: {
-//                 xs: 10,
-//                 sm: 5,
-//                 md: 3,
-//               },
-//               ml: 2,
-//               mr: 2,
-//             }}
-//           >
-//             <SearchField />
-//             <CreatePostButton />
-//           </Box>
-//           {error && (
-//             <Snackbar open autoHideDuration={6000}>
-//               <Alert severity="error">{error}</Alert>
-//             </Snackbar>
-//           )}
-//           {posts.length === 0 && (
-//             <Typography variant="h6" align="center">
-//               No posts available.
-//             </Typography>
-//           )}
-//           <>
-//             <PostList
-//               posts={posts}
-//               isLoading={isLoading}
-//               showEdit={isMyPosts ? true : false}
-//               showDelete={isMyPosts ? true : false}
-//             />
-//             <Box
-//               sx={{
-//                 display: "flex",
-//                 justifyContent: "center",
-//                 marginTop: 2,
-//                 mb: 5,
-//               }}
-//             >
-//               <Pagination
-//                 count={Math.ceil(total / limit)}
-//                 page={page}
-//                 onChange={handlePageChange}
-//                 color="primary"
-//                 sx={{ mb: { xs: 4, sm: 2 } }}
-//               />
-//             </Box>
-//           </>
-//         </Box>
-//       </Container>
-//     </Box>
-//   );
-// };
-
-// export default Posts;
-
 import React, { useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import {
@@ -143,16 +11,17 @@ import {
 import PostList from "../components/PostList";
 import useFetchPosts from "../hooks/useFetchPosts";
 import useFetchSearchPosts from "../hooks/useFetchSearchPosts";
-import { defaultPage } from "../utils/pagination";
+import { defaultLimit, defaultPage } from "../utils/pagination";
 import SearchField from "../components/SearchField";
 import CreatePostButton from "../components/CreatePostButton";
 import Sidebar from "../components/Sidebar";
 import RowsPerPageSelect from "../components/RowsPerPageSelect";
+import NoPostsMessage from "../components/NoPostsMessage";
 
 const Posts = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const page = parseInt(searchParams.get("page")) || defaultPage;
-  const limit = parseInt(searchParams.get("limit")) || 6; // Default limit
+  const limit = parseInt(searchParams.get("limit")) || defaultLimit;
   const searchQuery = searchParams.get("search") || "";
   const isMyPosts = searchParams.get("filter") === "my-posts";
 
@@ -246,11 +115,7 @@ const Posts = () => {
               <Alert severity="error">{error}</Alert>
             </Snackbar>
           )}
-          {posts.length === 0 && (
-            <Typography variant="h6" align="center">
-              No posts available.
-            </Typography>
-          )}
+          <NoPostsMessage isLoading={isLoading} posts={posts} />
           <>
             <PostList
               posts={posts}
